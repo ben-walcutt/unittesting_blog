81049ced3276bcfa229aa31bbc05d1c327b03262
Ben <ben.walcutt@gmail.com>
2020-09-16 18:56:56 -0500

    removed unused import

--- src/main/java/co/newlabs/AppService.java
+++ src/main/java/co/newlabs/AppService.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import ma.glasnost.orika.MapperFacade;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
